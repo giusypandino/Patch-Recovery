ba0e52f98fb25a11c7c05ddafa70b33cb4bf5500
giusypandino <g412mail2@gmail.com>
2024-12-29 10:11:03 +0100

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-if [ -f recovery.img.lz4 ];then
-	lz4 -B6 --content-size -f recovery.img.lz4 recovery.img
+# If the file 'download' exists, rename it to 'recovery.img'
+if [ -f download ]; then
+    mv download recovery.img
+    echo "File 'download' renamed to 'recovery.img'"
+else
+    echo "'download' file does not exist."
 fi
 
-off=$(grep -ab -o SEANDROIDENFORCE recovery.img |tail -n 1 |cut -d : -f 1)
+# The rest of your script can follow here...
+if [ -f recovery.img.lz4 ]; then
+    lz4 -B6 --content-size -f recovery.img.lz4 recovery.img
+fi
+
+off=$(grep -ab -o SEANDROIDENFORCE recovery.img | tail -n 1 | cut -d : -f 1)
 dd if=recovery.img of=r.img bs=4k count=$off iflag=count_bytes
 
-if [ ! -f phh.pem ];then
-    openssl genrsa -f4 -out phh.pem 4096
+if [ ! -f phh.pem ]; then
+    openssl genrsa -out phh.pem 4096
 fi
